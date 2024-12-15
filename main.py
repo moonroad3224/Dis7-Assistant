632fce26d43160aafd149e100541fef18626c326
moonroad3224 <moonload3224@gmail.com>
2024-12-15 10:41:20 +0900

    サーバーごとに設定を管理する形式の決定

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os,dotenv,datetime
+import os,dotenv,datetime,json
 import discord
 from pathlib import Path
 from discord import app_commands
@@ -10,9 +10,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 tree = app_commands.CommandTree(client)
 dotenv.load_dotenv()
 bottoken = os.environ.get("TOKEN1")
-reactionrole_title = ["A role", "B role", None, None, None]
-reactionrole_messageid = [1307162596394405928, 1307162596394405928, None, None, None]
-reactionrole_roleid = [1307163041137299476, 1307163064751226900, None, None, None]
 
 @client.event
 async def on_ready():
@@ -30,6 +27,7 @@ async def help(interaction: discord.Interaction):
     embed = discord.Embed(title="ヘルプ", description=text, color=0x2000FF)
     await interaction.response.send_message(embed=embed)
     print(f"{datetime.datetime.now()}:{interaction.id} Done.")
+
 #いったん別ファイルで使うつもりのコマンド↓
 
 @tree.command(name="hello", description="挨拶を返します")
@@ -38,27 +36,5 @@ async def hello(interaction: discord.Interaction):
     print(f"{datetime.datetime.now()}:{interaction.id} \n{interaction.type}/{cmdname}/{interaction.application_id} \n{interaction.guild}:{interaction.guild_id}")
     await interaction.response.send_message('こんにちは!')
     print(f"{datetime.datetime.now()}:{interaction.id} Done.")
-
-@client.event
-async def on_raw_reaction_add(self, payload:discord.RawReactionActionEvent):
-    for i in 4:
-        if payload.message_id == reactionrole_messageid[i]:
-            await payload.member.add_roles(payload.member.guild.get_role(reactionrole_roleid[i]))
-    i = 0
-
-@client.event
-async def on_raw_reaction_remove(payload):
-    for i in 4:
-        if payload.message_id == reactionrole_messageid[i] :
-            print(f"{datetime.date.now()}:client.event on_raw_reaction_add reactionrole")
-            emoji_name = payload.emoji.name
-            guild = client.get_guild(payload.guild_id)
-            channel = guild.get_channel()
-            if emoji_name == ":regional_indicator_a: ":
-                for role in guild.get_member(payload.user_id).roles:
-                    if role.id == reactionrole_roleid[i]:
-                        await guild.get_member(payload.user_id).remove_roles(guild.get_role(reactionrole_roleid[i]))#ロールを削除
-    i = 0
-
 
 client.run(bottoken)
